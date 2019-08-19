977ac5e0d4306b10193459e70727076d1f1387ad
Morgan Bentell <morgan.bentell@evry.com>
2019-08-19 19:50:09 +0200

    make testing macros compile to cljs using macrovich. It's possible to require the testing macros withuot require-macros from cljs.

--- project.clj
+++ project.clj
@@ -2,6 +2,8 @@
   :description "Useful tools for Clojure/ClojureScript"
   :url ""
   :license {}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/clojurescript "1.10.520"]
+                 [net.cgrand/macrovich "0.2.1"]]
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
---
+++ src/ysera/debug.cljc
@@ -0,0 +1,1 @@
+(ns ysera.debug)
