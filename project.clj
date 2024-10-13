4efb0303fafa8c7f0c34af221b017c39e1e3373d
Tomas Ekholm <ekholm.tomas@gmail.com>
2024-10-13 16:09:48 +0200

    Added deps dependencies

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject ysera "2.0.2"
+(defproject ysera "2.0.3"
   :description "Useful tools for Clojure/ClojureScript"
   :url ""
   :license {}
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.clojure/clojurescript "1.10.520"]
+  :dependencies [[org.clojure/clojure "1.12.0"]
+                 [org.clojure/clojurescript "1.11.132"]
                  [net.cgrand/macrovich "0.2.1"]]
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
